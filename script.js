591f6a86aab619a5893bdfbd02b40cb95fc6bb6a
homailali <138436251+homailali@users.noreply.github.com>
2023-08-16 09:02:29 -0700

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("article1__button").addEventListener("click",function(){
+    const linkurl='https://discord.gg/3KJKQpHYvE';
+    window.open(linkurl);
+})
+document.getElementById("article4__btn1").addEventListener("click",function(){
+    const linkurl='https://homailali.github.io/Usability-hub/';
+    window.open(linkurl);
+})
+document.getElementById("article4__btn2").addEventListener("click",function(){
+    const linkurl='https://homailali.github.io/fiver/';
+    window.open(linkurl);
+})
