11289c1dec6754c50e55ad5443f956a1ad627d64
homailali <138436251+homailali@users.noreply.github.com>
2023-09-10 08:24:09 +0500

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("article4__btn1");
+button.addEventListener("click", function() {
+    window.open("https://homailali.github.io/Usability-hub/", "_blank");
+});
+
+var button = document.getElementById("article4__btn2");
+button.addEventListener("click", function() {
+    window.open("https://homailali.github.io/fiver/", "_blank");
+});
+var button = document.getElementById("article1__button");
+button.addEventListener("click", function() {
+    window.open("https://discord.gg/3KJKQpHYvE", "_blank");
+});
